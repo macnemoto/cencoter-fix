7657300776ace6be7c5631370aee58d0acce77eb
Marcos Pirela <marcosj465@gmail.com>
2025-03-14 13:01:35 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+// import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='flex justify-center items-center h-dvh'>
+        <div>
+          <h1 className='text-7xl mb-[5rem]'>Corrector de palabras</h1>
+          <form action="">
+            <input className='py-2 pl-5 border border-white w-3xl' type="text" />
+          </form>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
