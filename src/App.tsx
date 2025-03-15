3384f14c25b34c19110c493ceac3e2ca8e3a48c6
macnemoto <marcosj465@gmail.com>
2025-03-15 14:52:50 -0400

    complete v1

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
-import { useState } from 'react'
+import { ChangeEvent, useState } from 'react'
 import { Buffer } from 'buffer'
 import { Clipboard } from 'lucide-react';
 
 import './App.css'
 
 function App() {
-  const [words, setWords] = useState(null)
+  const [words, setWords] = useState<string | null>(null)
 
-  const handleWords = (event) => {
+  const handleWords = (event : ChangeEvent<HTMLInputElement>) => {
     event.preventDefault()
     const codeFinal = handleCode(event.target.value)
     setWords(codeFinal)
     console.log(words)
   }
 
-  const handleCode = (code) => {
+  const handleCode = (code: string): string => {
     try {
       const bufferLatin1 = Buffer.from(code, 'latin1')
       const codeFix = bufferLatin1.toString('utf8')
@@ -27,17 +27,31 @@ function App() {
 
   }
 
+  const handleClick = () => {
+    try {
+      if (words != null) {
+        navigator.clipboard.writeText(words)
+      }
+    } catch (error) {
+    console.log('No hay nada que copiar')
+    return
+    }
+    console.log('funciono')
+  }
+
   return (
     <>
       <div className='flex justify-center items-center h-dvh'>
-        <div>
+        <div className=''>
           <h1 className='text-7xl mb-[5rem]'>Corrector de palabras</h1>
           <form action="" className='mb-[5rem]'>
             <input className='py-2 pl-5 border border-white w-3xl rounded' type="text" onChange={handleWords} />
           </form>
           <div className='flex bg-[#161618] p-5 rounded'>
-            <h2 className='text-cyan-500 text-5xl mr-3'>{words}</h2>
-            <button className='p-4 hover:border hover:border-white rounded'> <Clipboard /></button>
+            <h2 className='text-cyan-500 text-5xl mr-3 w-5/6'>{words}</h2>
+            <div className='w-1/6 flex justify-center items-center'>
+            <button className='p-4 hover:border hover:border-white rounded mx-2' onClick={handleClick}> <Clipboard /></button>
+            </div>
           </div>
         </div>
       </div>
